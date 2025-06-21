541fdd37e5be718102c3e66f083b0c57557eb890
motcaitenkhongtrungvoiai <hhieu3343@gmail.com>
2025-06-21 22:36:25 +0700

    thêm ghi chú nhỏ

--- service/sendContract.js
+++ service/sendContract.js
@@ -1,7 +1,10 @@
- 
-const YOUR_PUBLIC_KEY ="";
-const YOUR_SERVICE_ID ="";
-const YOUR_TEMPLATE_ID ="";
+/*
+ - load  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"> vào file html 
+ hoặc import từ  $npm install --save @emailjs/browser để dùng 
+ */
+const YOUR_PUBLIC_KEY = "";
+const YOUR_SERVICE_ID = "";
+const YOUR_TEMPLATE_ID = "";
 
 emailjs.init(YOUR_PUBLIC_KEY);
 
@@ -10,7 +13,8 @@ window.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contact-form");
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-    emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form)
+    emailjs
+      .sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form)
       .then(() => {
         console.log("SUCCESS!");
       })
